9033df88a60fafc5c722623e1a3545068c3817dc
deekshithas <sdeekshitha258@gmail.com>
2018-08-29 12:34:23 +0530

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,18 @@
+import cv2
+img=cv2.imread("flower.jpg")
+cv2.imshow("original image",img)
+cv2.waitKey(0)
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.waitKey(0)
+edged=cv2.Canny(gray,30,200)
+cv2.imshow("canned edge",edged)
+cv2.waitKey(0)
+_,contours,hierarchy=cv2.findContours(edged.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+cv2.imshow("canny edged after contouring",edged)
+cv2.waitKey(0)
+
+print("number of contours found",str(len(contours)))
+cv2.drawContours(img,contours,-1,(0,255,0),3)
+cv2.imshow("contours",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
